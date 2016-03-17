8efe07b0d8661e1cc8c33b438bf9809e931c1e17
Don Henton <donhenton@gmail.com>
2016-03-17 14:28:13 -0500

    working on build script

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +17,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build/assets')));
 
-app.use('/', routes);
-app.use('/users', users);
+ 
+ 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,55 @@
  * and open the template in the editor.
  */
 
-var gulp = require('gulp');
 
-gulp.task('default', function () {
-    // place code for your default task here
+var gulp = require('gulp');
+var del = require('del');
+var concat = require("gulp-concat");
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var replace = require('gulp-html-replace');
+var server = require('gulp-server-livereload');
+var gutil = require('gulp-util');
+
+gulp.task('clean', function ( ) {
+
+    del.sync(['build', 'reports']);
+
 });
+
+
+gulp.task('copy-resources', function () {
+
+
+
+    gulp.src(
+            ['./application/assets/app/*']
+
+
+            ).pipe(gulp.dest('./build/assets'));
+
+    gulp.src(
+            ['./application/assets/libs/jquery/dist/jquery.min.js']
+
+
+            ).pipe(gulp.dest('./build/assets/js/'));
+    
+    gulp.src(
+            ['./application/assets/libs/bootstrap/dist/**/*']
+
+
+            ).pipe(gulp.dest('./build/assets/bootstrap/dist/'));
+
+    gulp.src(
+            ['./application/*.html']
+
+
+            ).pipe(gulp.dest('./build'));
+
+
+
+});
+
+
+//gulp.task('build', ['clean', 'minify-copy-js', 'copy-resources','prepare-index-html']);
+gulp.task('build', ['clean', 'copy-resources']);
